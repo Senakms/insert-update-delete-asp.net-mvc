99da29a357f9c68edde871aaa340a0a18158973c
Senakms <36444310+Senakms@users.noreply.github.com>
2019-07-30 13:07:13 +0300

    asp.net mvc

---
+++ project01/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using project01.Models;
+using project01.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace project01.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        Db1Entities db = new Db1Entities();
+
+
+        public ActionResult Index()
+        {
+            List<Table> kayitListe = db.Table.ToList();
+            return View(kayitListe);
+        }
+        public ActionResult newRecord()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult newRecord(recorModel model)
+        {
+            Table record = new Table();
+            record.NameSurname = model.NameSurname;
+            record.Email = model.Email;
+            record.Age = model.Age;
+            record.Gsm = model.Gsm;
+            record.Address = model.Address;
+            record.City = model.City;
+            db.Table.Add(record);
+            db.SaveChanges();
+            ViewBag.result = "Record is done!";
+            return View();
+        }
+
+        public ActionResult deleteRecord(int? id)
+        {
+            Table record = db.Table.Where(k => k.Id == id).SingleOrDefault();
+            db.Table.Remove(record);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult updateRecord(int? id)
+        {
+            Table record = db.Table.Where(k => k.Id == id).SingleOrDefault();
+            recorModel model = new recorModel();
+            model.Id = record.Id;
+            model.NameSurname = record.NameSurname;
+            model.Email = record.Email;
+            model.Age = record.Age;
+            model.Gsm = record.Gsm;
+            model.Address = record.Address;
+            model.City = record.City;
+
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public ActionResult updateRecord(recorModel m)
+        {
+            Table record = db.Table.Where(k => k.Id == m.Id).SingleOrDefault();
+            record.NameSurname = m.NameSurname;
+            record.Email = m.Email;
+            record.Age = m.Age;
+            record.Address = m.Address;
+            record.City = m.City;
+            db.SaveChanges();
+            ViewBag.result = "Record is updated!";
+            return View();
+        }
+
+    }
+}
---
+++ project01/ViewModel/recorModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace project01.ViewModel
+{
+    public class recorModel
+    {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Enter Name Surname!")]
+        [Display(Name = "Name Surname")]
+        [StringLength(50, ErrorMessage = "Name Surname must be at most 50 characters !")]
+        public string NameSurname { get; set; }
+
+        [Required(ErrorMessage = "Enter email!")]
+        [Display(Name = "E-mail")]
+        [StringLength(50, ErrorMessage = "E-mail must be at most 50 characters!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Enter Age!")]
+        [Display(Name = "Age")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Enter GSM!")]
+        [Display(Name = "GSM")]
+        [StringLength(50, ErrorMessage = "Gsm must be at most 50 characters!")]
+        public string Gsm { get; set; }
+
+        [Required(ErrorMessage = "Enter Address!")]
+        [Display(Name = "Address")]
+        [StringLength(50, ErrorMessage = "Address must be at most 50 characters!")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Enter city!")]
+        [Display(Name = "City")]
+        [StringLength(50, ErrorMessage = "City must be at most 50 characters!")]
+        public string City { get; set; }
+    }
+}
